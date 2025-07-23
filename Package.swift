4c425ea68ff1b5f2e58a0d06337e8e60030deed0
5MonthGit <973853543@qq.com>
2025-07-23 10:16:41 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,10 +12,8 @@ let package = Package(
     ],
     products: [
         .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
-        .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     dependencies: [
-        .package(url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.8")),
         .package(url: "https://github.com/leeway1208/MqttCocoaAsyncSocket", from: "1.0.8"),
     ],
     targets: [
@@ -23,11 +21,6 @@ let package = Package(
                 dependencies: [ "MqttCocoaAsyncSocket" ],
                 path: "Source",
                 exclude: ["CocoaMQTTWebSocket.swift"],
-                swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
-        .target(name: "CocoaMQTTWebSocket",
-                dependencies: [ "CocoaMQTT", "Starscream" ],
-                path: "Source",
-                sources: ["CocoaMQTTWebSocket.swift"],
                 swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
         .testTarget(name: "CocoaMQTTTests",
                     dependencies: [ "CocoaMQTT", "CocoaMQTTWebSocket" ],
